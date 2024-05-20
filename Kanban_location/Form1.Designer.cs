0c48847a81557ca3f1c5d58c2996e94c401f15ec
SaberGammaoui <saber.gammaoui5@gmail.com>
2024-05-20 22:13:59 +0100

    last project update

--- Kanban_location/Form1.Designer.cs
+++ Kanban_location/Form1.Designer.cs
@@ -36,6 +36,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,61 +68,120 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(30, 300);
-            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(327, 47);
+            this.textBox1.Size = new System.Drawing.Size(567, 35);
             this.textBox1.TabIndex = 2;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(30, 406);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(30, 430);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(327, 47);
+            this.textBox2.Size = new System.Drawing.Size(140, 47);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 269);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 259);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 28);
+            this.label2.Size = new System.Drawing.Size(87, 38);
             this.label2.TabIndex = 4;
             this.label2.Text = "EPN :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 375);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 381);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 28);
+            this.label3.Size = new System.Drawing.Size(95, 38);
             this.label3.TabIndex = 5;
             this.label3.Text = "Type :";
             // 
             // listView1
             // 
-            this.listView1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(645, 300);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(615, 452);
             this.listView1.TabIndex = 6;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(640, 269);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(648, 259);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(124, 28);
+            this.label4.Size = new System.Drawing.Size(188, 38);
             this.label4.TabIndex = 7;
             this.label4.Text = "Localisation :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 529);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 38);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Color :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(35, 575);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(581, 10);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(35, 594);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(581, 10);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(359, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 38);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Section :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(366, 430);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(140, 47);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(196, 537);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 28);
+            this.label7.TabIndex = 13;
             // 
             // Form1
             // 
@@ -124,6 +189,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.ClientSize = new System.Drawing.Size(1288, 764);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label3);
@@ -135,7 +206,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -152,6 +222,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Kanban_location/Form1.cs
+++ Kanban_location/Form1.cs
@@ -14,7 +14,7 @@ namespace Kanban_location
 {
     public partial class Form1 : Form
     {
-        SqlConnection cx = new SqlConnection(@"Data Source=DESKTOP-QP22EHC\SQLEXPRESS;Initial Catalog=Kanban_location;User id=DESKTOP-QP22EHC\admis;Password=;");
+        SqlConnection cx = new SqlConnection(@"Data Source=DESKTOP-QP22EHC\SQLEXPRESS;Initial Catalog=Kanban_location;Integrated Security=True");
         public Form1()
         {
             InitializeComponent();
@@ -25,16 +25,126 @@ namespace Kanban_location
         {
             if (e.KeyChar == Convert.ToChar(Keys.Enter))
             {
-
+                listView1.Clear();
                 cx.Open();
-                SqlCommand cmd = new SqlCommand("select Type  From Table_location where Material='" + textBox1.Text + "'",cx) ;
+                SqlCommand cmd = new SqlCommand("select *  From Location where Material='" + textBox1.Text + "'",cx) ;
                 SqlDataReader reader = cmd.ExecuteReader() ;
-                if (reader.Read())
+                bool hasrows = false;
+                while (reader.Read())
                 {
+                    hasrows = true;
                     textBox2.Text = reader["Type"].ToString() ;
+                    listView1.Items.Add(reader["Location"].ToString());
+                    textBox5.Text = reader["Section"].ToString();
+                    string color1 = reader["Color1"].ToString();
+                    string color2 = reader["Color2"].ToString();
+                    label7.Text = color1 +" | "+color2;
+                    switch(color1.ToUpper())
+                    {
+                        case "RD":
+                            textBox3.BackColor = Color.Red;                       
+                            break;
+                        case "YE":
+                            textBox3.BackColor = Color.Yellow;                                               
+                            break;
+                        case "BU":
+                            textBox3.BackColor = Color.Blue;
+                            break;
+                        case "WH":
+                            textBox3.BackColor = Color.White;
+                            break;
+                        case "BN":
+                            textBox3.BackColor = Color.Brown;
+                            break;
+                        case "VT":
+                            textBox3.BackColor = Color.Purple;
+                            break;
+                        case "GN":
+                            textBox3.BackColor = Color.Green;
+                            break;
+                        case "PK":
+                            textBox3.BackColor = Color.Pink;
+                            break;
+                        case "BK":
+                            textBox3.BackColor = Color.Black;
+                            break;
+                        case "GY":
+                            textBox3.BackColor = Color.Gray;
+                            break;
+                        case "OG":
+                            textBox3.BackColor = Color.Orange;
+                            break;
+                        case "BG":
+                            textBox3.BackColor = Color.Beige;
+                            break;
+
+                        default:
+                            textBox3.BackColor = Color.Transparent;
+                            break;
+
+
+                    }
+                    switch (color2.ToUpper())
+                    {
+                        case "RD":
+                            textBox4.BackColor = Color.Red;
+                            break;
+                        case "YE":
+                            textBox4.BackColor = Color.Yellow;
+                            break;
+                        case "BU":
+                            textBox4.BackColor = Color.Blue;
+                            break;
+                        case "WH":
+                            textBox4.BackColor = Color.White;
+                            break;
+                        case "BN":
+                            textBox4.BackColor = Color.Brown;
+                            break;
+                        case "VT":
+                            textBox4.BackColor = Color.Purple;
+                            break;
+                        case "GN":
+                            textBox4.BackColor = Color.Green;
+                            break;
+                        case "PK":
+                            textBox4.BackColor = Color.Pink;
+                            break;
+                        case "BK":
+                            textBox4.BackColor = Color.Black;
+                            break;
+                        case "GY":
+                            textBox4.BackColor = Color.Gray;
+                            break;
+                        case "OG":
+                            textBox4.BackColor = Color.Orange;
+                            break;
+                        case "BG":
+                            textBox4.BackColor = Color.Beige;
+                            break;
+
+                        default:
+                            textBox4.BackColor = Color.Transparent;
+                            break;
+
+
+                    }
                 }
-                reader.Close();
+                reader.Close();             
+                cx.Close();
+                if (!hasrows)
+                {
+                    MessageBox.Show("The entered value does not exist");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox5.Clear();
+                    textBox3.BackColor = Color.White;
+                    textBox4.BackColor = Color.White;
+                    textBox1.Focus();
+
+                }
             }
         }
+
     }
 }
